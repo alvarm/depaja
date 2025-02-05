5182753475962da102a035277b9002ff0255f83c
Àlvar Mercadé Ibáñez <alvar.mercade@gmail.com>
2025-02-05 12:45:23 +0100

    Code is written. Only pending is documentation.

---
+++ src/main/java/com/github/alvarm/depaja/model/Catalogue.java
@@ -0,0 +1,41 @@
+/*
+ * MIT License
+ * Copyright 2025 Àlvar Mercadé Ibáñez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.alvarm.depaja.model;
+
+public class Catalogue {
+    private final Truck basicTruck;
+
+    private Catalogue() {
+        basicTruck = new EuropeanTruck(new Model("Global Truck", "Test Truck"),
+                new Engine(new Model("Global Truck", "v22-Revo"), 500),
+                new Wheel(new Model("RubberRun", "RoadRunner"), 20));
+    }
+
+    public static Catalogue getCatalogue() {
+        return CatalogueHolder.INSTANCE;
+    }
+
+    public Truck getBasicTruck() {
+        return basicTruck;
+    }
+
+    private static class CatalogueHolder {
+        private static final Catalogue INSTANCE = new Catalogue();
+    }
+}
