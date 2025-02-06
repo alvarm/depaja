0aa73c30b60aa54fa32c42c9bd2ccb2459884429
Àlvar Mercadé Ibáñez <alvar.mercade@gmail.com>
2025-02-06 09:58:56 +0100

    Code is written. Truck class is documented. Added changes to pom.xml in order to execute Main class.

--- src/main/java/com/github/alvarm/depaja/model/Truck.java
+++ src/main/java/com/github/alvarm/depaja/model/Truck.java
@@ -20,57 +20,142 @@ package com.github.alvarm.depaja.model;
 
 import java.util.Objects;
 
+/**
+ * Abstract class that represents a truck.<br>
+ * This class implements the <b>Prototype</b> design pattern.
+ *
+ * @author alvarm
+ * @version 1.0
+ * @see Cloneable
+ */
 public abstract class Truck implements Cloneable {
 
     private Model modelName;
-    private Engine motor;
+    private Engine engine;
     private Wheel wheel;
 
-    protected Truck(Model modelName, Engine motor, Wheel wheel) {
+    /**
+     * Constructor method the sets up all its attributes.
+     *
+     * @param modelName The model for the new truck.
+     * @param engine    The engine for the new truck.
+     * @param wheel     The wheel model for the new truck.
+     * @see Model
+     * @see Engine
+     * @see Wheel
+     * @since 1.0
+     */
+    protected Truck(Model modelName, Engine engine, Wheel wheel) {
         this.modelName = modelName;
-        this.motor = motor;
+        this.engine = engine;
         this.wheel = wheel;
     }
 
+    /**
+     * Returns the model name for this truck.
+     *
+     * @return The model name.
+     * @see Model
+     * @since 1.0
+     */
     public Model getModelName() {
         return modelName;
     }
 
-    public void setModelName(Model modelName) {
+    /**
+     * Sets a new model name for this truck.
+     *
+     * @param modelName The new model name.
+     * @see Model
+     * @since 1.0
+     */
+    protected void setModelName(Model modelName) {
         this.modelName = modelName;
     }
 
-    public Engine getMotor() {
-        return motor;
+    /**
+     * Returns the engine used in this truck.
+     *
+     * @return The engine used for this model.
+     * @see Engine
+     * @since 1.0
+     */
+    public Engine getEngine() {
+        return engine;
     }
 
-    public void setMotor(Engine motor) {
-        this.motor = motor;
+    /**
+     * Sets a new engine model for this truck.
+     *
+     * @param engine The new engine for this model.
+     * @see Engine
+     * @since 1.0
+     */
+    protected void setEngine(Engine engine) {
+        this.engine = engine;
     }
 
+    /**
+     * Returns the wheel model used for this truck.
+     *
+     * @return The wheel model for this truck.
+     * @see Wheel
+     * @see Wheel
+     * @since 1.0
+     */
     public Wheel getWheel() {
         return wheel;
     }
 
-    public void setWheel(Wheel wheel) {
+    /**
+     * Sets a new wheel model for this truck.
+     *
+     * @param wheel The new wheel model.
+     * @see Wheel
+     * @since 1.0
+     */
+    protected void setWheel(Wheel wheel) {
         this.wheel = wheel;
     }
 
+    /**
+     * Calculates a hash code based on the instance fields.
+     *
+     * @return A hash code for this instance.
+     * @see Object#hashCode()
+     * @since 1.0
+     */
     @Override
     public int hashCode() {
         int result = Objects.hashCode(modelName);
-        result = 31 * result + Objects.hashCode(motor);
+        result = 31 * result + Objects.hashCode(engine);
         result = 31 * result + Objects.hashCode(wheel);
         return result;
     }
 
+    /**
+     * Check is a truck is equals to another object.
+     *
+     * @param o An object to be compared with.
+     * @return <i>true</i>, if <code>o</code> is a Truck and has the same attributes.<br>
+     * <i>false</i>, otherwise.
+     * @since 1.0
+     */
     @Override
     public final boolean equals(Object o) {
         if (!(o instanceof Truck truck)) return false;
 
-        return Objects.equals(modelName, truck.modelName) && Objects.equals(motor, truck.motor) && Objects.equals(wheel, truck.wheel);
+        return Objects.equals(modelName, truck.modelName) && Objects.equals(engine, truck.engine) && Objects.equals(wheel, truck.wheel);
     }
 
+    /**
+     * Returns a copy of a truck.
+     * This method is part of the <b>Prototype</b> pattern.
+     *
+     * @return A copy of <code>this</code> truck.
+     * @see Object#clone()
+     * @since 1.0
+     */
     @Override
     public Truck clone() {
         try {
